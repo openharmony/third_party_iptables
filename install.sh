35c3f8db605b2e2522e4fb384da54f365b68ceaf
maosiping <maosiping@huawei.com>
2023-04-21 09:55:31 +0800

    add /bin/bash
    Signed-off-by: maosiping <maosiping@huawei.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright (c) 2022 Huawei Device Co., Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/bash
 
 set -e
 
